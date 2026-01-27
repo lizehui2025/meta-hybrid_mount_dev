4266effe98460f7822eac12ae23f30986e438241
Imagining_Time_and_Space <zehuili200756@gmail.com>
2026-01-27 18:39:32 +0800

    Update defs.rs

--- src/defs.rs
+++ src/defs.rs
@@ -2,7 +2,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 pub const DEFAULT_HYBRID_MNT_DIR: &str = "/debug_ramdisk";
-pub const MODULES_IMG_FILE: &str = "/data/adb/meta-hybrid/modules.img";
+pub const WORKSPACE_MNT: &str = "/mnt/vendor/meta-hybrid"; 
+pub const SYSTEM_RW_DIR: &str = "/dev/meta-hybrid/upper";
 pub const RUN_DIR: &str = "/data/adb/meta-hybrid/run/";
 pub const STATE_FILE: &str = "/data/adb/meta-hybrid/run/daemon_state.json";
 pub const DISABLE_FILE_NAME: &str = "disable";
